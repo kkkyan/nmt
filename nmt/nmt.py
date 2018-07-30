b9ca1f0c7bfaea87282eb31c46a86d9231380026
yankai <kaiyanchn@foxmail.com>
2018-07-30 17:25:52 +0800

    save server

--- nmt/nmt.py
+++ nmt/nmt.py
@@ -24,6 +24,8 @@ import sys
 # import matplotlib.image as mpimg
 import numpy as np
 import tensorflow as tf
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 from . import inference
 from . import train
