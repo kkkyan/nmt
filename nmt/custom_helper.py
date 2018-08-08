ef7e13a4e34be88c8b27eaa168c359e61878a7a5
kky <kaiyanchn@foxmail.com>
2018-08-08 20:38:32 +0800

    future

--- nmt/custom_helper.py
+++ nmt/custom_helper.py
@@ -1,5 +1,3 @@
-import tensorflow as tf 
-from tensorflow.contrib.seq2seq.python.ops.decoder import Decoder
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -8,6 +6,8 @@ import abc
 import six
 
 # pylint: disable-msg=E0611
+import tensorflow as tf 
+from tensorflow.contrib.seq2seq.python.ops.decoder import Decoder
 from tensorflow.python.framework import constant_op
 from tensorflow.python.framework import dtypes
 from tensorflow.python.framework import ops
